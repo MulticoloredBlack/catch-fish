c388ddb50334de292e844c78b3d0e4a63c98d3fd
byodian <byj6696@gmail.com>
2021-12-19 10:06:57 +0800

    feat: Update .eslintrc.js

--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,14 @@
-
-import * as vscode from 'vscode';
-const fs = require('fs');
-import { exec } from 'child_process';
 export function getNow() {
-	let dateTime;
-	let yy = new Date().getFullYear();
-	let mm = new Date().getMonth() + 1;
-	let dd = new Date().getDate();
-	let hh = new Date().getHours();
-	let mf = new Date().getMinutes() < 10 ? '0' + new Date().getMinutes()
-		:
-		new Date().getMinutes();
-	let ss = new Date().getSeconds() < 10 ? '0' + new Date().getSeconds()
-		:
-		new Date().getSeconds();
-	dateTime = yy + '-' + mm + '-' + dd + ' ' + hh + ':' + mf + ':' + ss;
-	return dateTime;
+  const yy = new Date().getFullYear();
+  const mm = new Date().getMonth() + 1;
+  const dd = new Date().getDate();
+  const hh = new Date().getHours();
+  const mf = new Date().getMinutes() < 10 ? '0' + new Date().getMinutes()
+    :
+    new Date().getMinutes();
+  const ss = new Date().getSeconds() < 10 ? '0' + new Date().getSeconds()
+    :
+    new Date().getSeconds();
+  const dateTime = yy + '-' + mm + '-' + dd + ' ' + hh + ':' + mf + ':' + ss;
+  return dateTime;
 }
